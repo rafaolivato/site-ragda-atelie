b2945ee5351be5fedeb89c02b804eaed3aabff0c
rafaolivato <rafaolivato82@gmail.com>
2026-06-09 15:11:18 -0300

    Mudança de imagem

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ export default function App() {
             backgroundSize: 'cover', backgroundPosition: 'center', borderRadius: '8px' , marginTop: '40px'
           }} title="Pijamas Premium"></div>
           <div style={{ 
-            backgroundImage: 'url("https://images.unsplash.com/photo-1518310383802-640c2de311b2?q=80&w=600")', 
+            backgroundImage: 'url("https://images.unsplash.com/photo-1630930678172-63343537a00a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bmVnJUMzJUIzY2lvcyUyMG0lQzMlQTFxdWluYXMlMjBkZSUyMGNvc3R1cmF8ZW58MHx8MHx8fDA%3D")', 
             backgroundSize: 'cover', backgroundPosition: 'center', borderRadius: '8px', marginTop: '40px'
           }} title="Moda Esportiva / Leggings"></div>
         </div>
